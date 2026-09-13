9cc26fd0d3871bf8c96cc2ec8ff3e1c4db4a1b14
namnkahn1607 <nlnkandthebois@gmail.com>
2026-09-13 11:26:47 +0700

    feat: jeff dean

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Unsatisfied with constant time, Jeff Dean created the "
+                 "world's first O(1/n) algorithm."
+              << std::endl;
+    return 0;
+}

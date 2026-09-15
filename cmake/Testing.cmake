bb9c7f5eddb12ec851b06864cf4b09a66ea500fc
namnkahn1607 <nlnkandthebois@gmail.com>
2026-09-15 15:36:03 +0700

    fix: use CURRENT source and binary directory specifier instead of global ones

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -7,7 +7,9 @@
 function(add_unit_test target_name module_name)
     cmake_parse_arguments(ARG "" "" "SOURCES;LIBS" ${ARGN})
     add_executable(${target_name} ${ARG_SOURCES})
-    target_include_directories(${target_name} PRIVATE ${CMAKE_SOURCE_DIR}/src/${module_name})
+    target_include_directories(${target_name}
+        PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/src/${module_name}
+    )
     target_link_libraries(${target_name} PRIVATE ${ARG_LIBS} gtest_main gtest)
     gtest_discover_test(${target_name})
 endfunction()
